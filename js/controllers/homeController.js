cddcfe16441fa1e36fcd4c3ba92def1852e534c8
ginhanlabs <ginhanlabs@gmail.com>
2014-01-08 00:37:47 -0500

    still converting to bootstrap 3

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,7 +1,7 @@
 angular.module('cbInv.homeController', [])
   .controller('summaryController', function($scope){
-        $scope.valueByPublisher = {};
-
+        $scope.totalValueByPublisher = "705";
+        $scope.totalIssues = "395";
 
      /*   $scope.valueByPublisher["Marvel"] = "3344";
         $scope.valueByPublisher["DC"] = "1010"*/
@@ -9,7 +9,13 @@ angular.module('cbInv.homeController', [])
         // same as sideNavModel
         $scope.PublishersList = [
             {"id":1, "publisherId": 1, "publisherName": "Marvel", "value":"343"},
-            {"id":2, "publisherId": 2, "publisherName": "DC", "value":239},
+            {"id":2, "publisherId": 2, "publisherName": "DC", "value":"239"},
             {"id":3, "publisherId": 3, "publisherName": "Image", "value":"123"}
         ]
-  });
+
+        $scope.TotalIssuesPerTitle = [
+            {"id":1, "titleId":"1", "totalIssues":"200"},
+            {"id":2, "titleId":"2", "totalIssues":"55"},
+            {"id":3, "titleId":"3", "totalIssues":"140"}
+        ]
+    });
